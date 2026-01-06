d30ca99e2c56844307d59fde5d7c74582ea5fe5c
mohammedrameesu53-star <mohammedrameesu53@gmail.com>
2026-01-06 13:58:30 +0530

    Time: 176 ms (5.22%), Space: 67.2 MB (41.78%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    nums.sort() 
+    for(i=0;i<nums.length;i++){
+      if(nums[i]===nums[i+1]){
+        return nums[i]
+      }
+         
+    }
+
+
+};
