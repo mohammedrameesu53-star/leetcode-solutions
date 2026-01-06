b29653dcd409651285fbecacff7d76f54cb41e51
mohammedrameesu53-star <mohammedrameesu53@gmail.com>
2026-01-06 22:13:45 +0530

    Time: 0 ms (100%), Space: 56.5 MB (92.21%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+    let a = []
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            a.push("FizzBuzz")
+        } else if (i % 3 == 0) {
+            a.push("Fizz")
+        } else if (i % 5 == 0) {
+            a.push("Buzz")
+        } else {
+            a.push(String(i))
+        }
+    }
+
+    return a
+};
