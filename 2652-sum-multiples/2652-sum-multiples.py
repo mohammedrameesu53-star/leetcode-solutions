9649c26c30129312babdb1766202fb1ff0680d29
mohammedrameesu53-star <mohammedrameesu53@gmail.com>
2026-01-19 09:43:47 +0530

    Time: 29 ms (77.25%), Space: 12.4 MB (84.55%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sumOfMultiples(self, n):
+        x=0
+        for i in range(1,n+1):
+            if i%3 == 0 or i%5 == 0 or i%7 == 0:
+                x += i
+
+        return x        
+        """
+        :type n: int
+        :rtype: int
+        """
+        
